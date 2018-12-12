fcdb76a722f1c601ba09d26134c6cf1dfa0a8764
Marcy Silverman <marcysilve225@gmail.com>
2018-12-11 22:03:07 -0800

    set up calculator file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator'; 
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="calculator">
+    <div class="display"> Display </div>
+    <div> C </div>
+    <div> +/- </div>
+    <div> % </div>
+    <div> ÷ </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+    <div> Hello </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.calculator {
+  font-size: 20pt;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); 
+  grid-auto-rows: minmax(50px, auto); 
+}
+.display {
+  grid-column: 1 / 5;
+  background-color: red;
+  text-align: center;
+}
+</style>
